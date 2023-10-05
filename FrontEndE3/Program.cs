4e94e5623a24d92f36e77f853e365965f4bead6e
Pablo Gerardo Herrera Garay <pablo.herrera@elcondor.com>
2023-10-05 07:36:36 -0500

    Se construye base de datos y Api

--- FrontEndE3/Program.cs
+++ FrontEndE3/Program.cs
@@ -1,7 +1,12 @@
+using FrontEndE3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BaseDeDatosContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("db")));
 
 var app = builder.Build();
 
@@ -10,7 +15,23 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseRouting();
 
-//app.MapGet("/", () => "Hello World!");
+app.MapGet("/Api/Producto", async (BaseDeDatosContext con, [FromQuery] int? idCategoria, [FromQuery] string? buscar, [FromQuery] decimal? desdePrecio, [FromQuery] decimal? hastaPrecio, [FromQuery] bool? soloDestacados) =>
+{
+    var productos = await con.Producto.Where(p =>
+        (idCategoria == null || idCategoria == 0 || p.IdCategoria == idCategoria)
+        && (buscar == null || buscar == "" || p.Nombre.Contains(buscar))
+        && (desdePrecio == null || desdePrecio == 0 || p.Precio >= desdePrecio)
+        && (hastaPrecio == null || hastaPrecio == 0 || p.Precio <= hastaPrecio)
+        && (soloDestacados == null || soloDestacados == false || p.Destacado)
+    ).ToListAsync();
+    return productos;
+});
+
+app.MapGet("/Api/Categoria", async (BaseDeDatosContext con) =>
+{
+    var categorias = await con.Categoria.ToListAsync();
+    return categorias;
+});
 
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
--- FrontEndE3/wwwroot/js/app.js
+++ FrontEndE3/wwwroot/js/app.js
@@ -1,1 +1,64 @@
 ﻿
+function ApiClient() {
+    let baseUrl = "/Api/";
+    function crearQuery(data) {
+        if (data == null) return '';
+        else if (typeof (data) == 'string') {
+            if (data.length > 0) {
+                if (!data.startsWith('?')) data = '?' + data;
+                return data;
+            } else return '';
+        } else if (typeof (data) == 'object') {
+            let ar = [];
+            for (let e in data) {
+                let v = data[e];
+                if (v == null) continue;
+                ar.push(e + '=' + v);
+            }
+            let s = ar.join('&');
+            return crearQuery(s);
+        } else return '';
+    }
+    this.get = function (metodo, queryData) {
+        let url = baseUrl + metodo + crearQuery(queryData);
+        return new Promise((res, rej) => {
+            $.ajax({
+                url: url,
+                type: 'get',
+                dataType: 'json',
+                contentType: 'application/json',
+                success: d => res(d),
+                error: e => rej(e)
+            })
+        })
+    }
+    this.post = function (metodo, data, queryData) {
+        let url = baseUrl + metodo + crearQuery(queryData);
+        let jsonData = data == null ? '{}' : JSON.stringify(data);
+        return new Promise((res, rej) => {
+            $.ajax({
+                url: url,
+                type: 'post',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: jsonData,
+                success: d => res(d),
+                error: e => rej(e)
+            })
+        })
+    }
+    this.getCategorias = async function () {
+        return await this.get('Categoria');
+    }
+    this.getProductos = async function (idCategoria, buscar, desdePrecio, hastaPrecio, soloDestacados) {
+        return await this.get('Producto', {
+            idCategoria: idCategoria,
+            buscar: buscar,
+            desdePrecio: desdePrecio,
+            hastaPrecio: hastaPrecio,
+            soloDestacados: soloDestacados
+        })
+    }
+}
+
+globalThis.api = new ApiClient();
